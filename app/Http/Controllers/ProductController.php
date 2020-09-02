b726b0c4249382967e03db047bdedebf94b1450f
thanhbtpc00329 <51738173+thanhbtpc00329@users.noreply.github.com>
2020-09-02 19:34:29 +0700

    showproduct

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+class ProductController extends Controller
+{
+    public function showProduct()
+    {
+    	return Product::all();
+    }
+}
---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $primaryKey="product_id";
+    protected $table="products";
+    protected $fillable=['name','brand_id','cate_id','introduction','description','tag','shop_id','shop','status'];
+    public $timestamps=true;
+}
