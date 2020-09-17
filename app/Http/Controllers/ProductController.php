5a3391b0b16e6571013bc44b51c86d503cdb27e3
thanhbtpc00329 <51738173+thanhbtpc00329@users.noreply.github.com>
2020-09-17 18:24:53 +0700

    kk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -169,10 +169,10 @@ class ProductController extends Controller
             ->join('shops','shops.shop_id','=','products.shop_id')
             ->join('product_info','product_info.product_id','=','products.product_id')
             ->where('products.product_id','=',$id)
-            ->select('products.product_id','products.product_name','products.introduction','products.description','products.tag','brands.brand_name','categories.cate_name','categories.category','product_detail.prodetail_id','product_detail.price','product_detail.color','product_detail.quantity','product_detail.size','product_detail.discount_price','product_detail.status','product_image.image','shops.shop_name','address','shops.phone_number')
+            ->select('products.product_id','products.product_name','products.introduction','products.description','products.tag','brands.brand_name','categories.cate_name','categories.category','product_detail.prodetail_id','product_detail.price','product_detail.color','product_detail.quantity','product_detail.size','product_detail.discount_price','product_detail.status','product_image.image','shops.shop_name','address','shops.phone_number','product_info.origin','product_info.accessorry','product_info.dimention','product_info.weight','product_info.system','product_info.material','product_info.screen_size','product_info.wattage','product_info.resolution','product_info.memory')
             ->get();
 
-        return $detail;
+        return response()->json($detail);
     }
 
     public function test(){
--- database/migrations/2020_09_17_082708_create_product_info_table.php
+++ database/migrations/2020_09_17_082708_create_product_info_table.php
@@ -16,13 +16,16 @@ class CreateProductInfoTable extends Migration
         Schema::create('product_info', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->length(10)->unsigned();
-            $table->string('origin');
-            $table->string('accessorry');
-            $table->string('dimention');
-            $table->string('weight');
-            $table->string('system');
-            $table->string('material');
-
+            $table->string('origin')->nullable();
+            $table->string('accessorry')->nullable();
+            $table->string('dimention')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('system')->nullable();
+            $table->string('material')->nullable();
+            $table->string('screen_size')->nullable();
+            $table->string('wattage')->nullable();
+            $table->string('resolution')->nullable();
+            $table->string('memory')->nullable();
             $table->timestamps();
 
             $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade');
