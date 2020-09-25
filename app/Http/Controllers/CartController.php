8ec75d9cb0f804b03635d65a9f9adcb41628d401
thanhbtpc00329 <51738173+thanhbtpc00329@users.noreply.github.com>
2020-09-25 14:41:24 +0700

    sdsfsf

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,31 +17,17 @@ class CartController extends Controller
 		$cart1 = DB::table('carts')
 				->join('cart_detail','carts.cart_id','=','cart_detail.cart_id',)
 				->join('product_detail','cart_detail.prodetail_id','=','product_detail.prodetail_id')
+				->join('products','product_detail.product_id','=','products.product_id')
 				->join('users','users.user_id','=','carts.user_id')
 				->join('shops','shops.shop_id','=','cart_detail.shop_id')
 				->join('product_image','product_image.prodetail_id','=','product_detail.prodetail_id')
             	->groupby('product_image.prodetail_id')
 				->where('carts.user_id','=',$user_id)
-				->select('shops.shop_name','cart_detail.prodetail_id','product_image.image')
+				->select('products.product_id','products.product_name','products.introduction','products.description','products.tag','products.brand','product_detail.prodetail_id','product_detail.price','product_detail.color','product_detail.quantity','product_detail.size','product_detail.discount_price','product_detail.status','product_image.image','shops.shop_id','shops.shop_name','shops.address','shops.phone_number','product_detail.origin','product_detail.accessory','product_detail.dimension','product_detail.weight','product_detail.system','product_detail.material','product_detail.screen_size','product_detail.wattage','product_detail.resolution','product_detail.memory','users.avatar')
 				->get();
-		$kr=array();
-		for ($i=0; $i < count($cart1); $i++) { 
-			$cart2 = DB::table('products')
-            ->join('product_detail','product_detail.product_id','=','products.product_id')
-            ->where('product_detail.prodetail_id','=',$cart1[$i]->prodetail_id)
-            ->get();
-            if ($cart2) {
-            	array_push($kr,$cart2); 	
-            }
-		}
-		$kq = [
-			'shop_name' => $cart1[0]->shop_name,
-			'product_in_shop'=>$kr
-		];
-		$str1 = json_encode($kq);
-		$str2 = str_replace(array('[[',']]','],['),array('[',']',','),$str1);
-		$str3 = json_decode($str2);
-		return response()->json($str3);
+
+		
+		return response()->json($cart1);
 	}
 
 
@@ -54,14 +40,11 @@ class CartController extends Controller
             }
         $id = 'Cart_'.$rd;
         $time = now()->timezone('Asia/Ho_Chi_Minh');
-        $cart = $request->cart;
-        $arr = json_decode($cart);
-        for ($i=0; $i < count($arr); $i++) { 
-        	$user = $arr[$i]->user_id;
-        	$shop = $arr[$i]->shop_id;
-        }
-        $user_id = $user;
-        $shop_id = $shop;
+  //       $cart = $request->cart;
+		// $arr = json_decode($cart);
+        $prodetail_id = $request->prodetail_id;
+        $user_id = $request->user_id;
+        $shop_id = $request->shop_id;
         $cart = new Cart;
         $cart->cart_id = $id;
         $cart->user_id = $user_id;
@@ -69,15 +52,12 @@ class CartController extends Controller
 
         $cart->save();
 
-        for ($i=0; $i < count($arr); $i++) { 
-
-        	$cart_detail = new Cart_detail;
+        $cart_detail = new Cart_detail;
         	$cart_detail->cart_id = $id;
-        	$cart_detail->prodetail_id = $arr[$i]->prodetail_id;
+        	$cart_detail->prodetail_id = $prodetail_id;
         	$cart_detail->shop_id = $shop_id;
         	$cart_detail->created_at = $time;
         	$cart_detail->save();
-        }
 
         if ($cart_detail) {
         	echo "Thành công";
@@ -86,5 +66,65 @@ class CartController extends Controller
         	echo "Thất bại";
         }
 
- 	}   
+ 	}
+
+
+
+
+
+
+
+
+ 	public function Cart(Request $request){
+		$user_id = $request->user_id;
+
+		$cart1 = DB::table('carts')
+				->join('cart_detail','carts.cart_id','=','cart_detail.cart_id',)
+				->join('product_detail','cart_detail.prodetail_id','=','product_detail.prodetail_id')
+				->join('users','users.user_id','=','carts.user_id')
+				->join('shops','shops.shop_id','=','cart_detail.shop_id')
+				->join('product_image','product_image.prodetail_id','=','product_detail.prodetail_id')
+            	->groupby('product_image.prodetail_id')
+				->where('carts.user_id','=',$user_id)
+				->select('cart_detail.shop_id','cart_detail.prodetail_id','product_image.image','shops.shop_name')
+				->get();
+		$cart3 = DB::table('carts')
+				->join('cart_detail','carts.cart_id','=','cart_detail.cart_id',)
+				->join('shops','shops.shop_id','=','cart_detail.shop_id')
+				->where('carts.user_id','=',$user_id)
+				->select('cart_detail.shop_id','shops.shop_name')
+				->distinct()
+				->get();
+		$kr = array();
+		$tr = array();
+		
+		
+		$uu = array();
+		for ($i=0; $i < count($cart3); $i++) { 
+			$cart5 = Cart_detail::where('shop_id',$cart3[$i]->shop_id)->get();
+			for ($j=0; $j < count($cart5); $j++) { 
+				$cart2 = DB::table('product_detail')
+            ->join('products','product_detail.product_id','=','products.product_id')
+            ->join('shops','shops.shop_id','=','products.shop_id')
+            ->where('product_detail.prodetail_id','=',$cart1[$j]->prodetail_id)
+            ->where('product_detail.shop_id','=',$cart3[$i]->shop_id)
+            
+            ->get();
+            
+            	array_push($tr,$cart2);
+			}
+			
+			$kq = array(
+				'shop_name' => $cart3[$i]->shop_name,
+				'product_in_shop'=>$tr
+				); 
+				array_push($kr,$kq);
+			array_splice($tr,0,$j);
+		}
+		
+		// $str1 = json_encode($kr);
+		// $str2 = str_replace(array('[[',']]','],['),array('[',']',','),$str1);
+		// $str3 = json_decode($str2);
+		return $kr;
+	}   
 }
