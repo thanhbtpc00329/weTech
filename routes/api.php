d09946300a38001daa4428c55403a0de2571e311
thanhbtpc00329 <51738173+thanhbtpc00329@users.noreply.github.com>
2020-10-09 14:04:20 +0700

    dfgdfgdfgdfg

--- routes/api.php
+++ routes/api.php
@@ -109,6 +109,10 @@ Route::post('/shop-unactive','ShopController@accountShopUnactive');// Lấy tài
 Route::post('/show-shop','ShopController@showShop');// Lấy all thông tin shop
 Route::post('/get-shop','ShopController@getShop');// Lấy all thông tin shop đã duyệt
 Route::post('/unactive-shop','ShopController@unactiveShop');// Lấy all thông tin shop chờ duyệt
+Route::post('/block-shop','ShopController@blockShop');// Lấy all thông tin shop đã chặn
+
+Route::post('/cancel-shop','ShopController@cancelShop');// Chặn shop
+
 Route::post('/detail-shop','ShopController@detailShop');// Chi tiết shop theo id
 Route::post('/add-shop','ShopController@addShop');// Đăng ký shop
 
