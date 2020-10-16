159685fcbe87f78dc278a923b62d48d056adcced
thanhbtpc00329 <51738173+thanhbtpc00329@users.noreply.github.com>
2020-10-16 17:42:14 +0700

    dfdfhdfhdfh

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,12 +7,12 @@
 <body>
 	<form action="/api/post" method="POST" enctype="multipart/form-data">
 		<div class="form-group col-md-12">
-         <label>Content</label>
-         <textarea name="txtContent" class="form-control " id="editor1"></textarea>
+         <label>Date</label>
+         <input type="date" name="day" >
 		</div> 		
         <input type="submit" name="save" value="SAVE">
 	</form>
 	<script src="/ckeditor/ckeditor.js"></script>
-<script> CKEDITOR.replace('editor1'); </script>
+{{-- <script> CKEDITOR.replace('editor1'); </script> --}}
 </body>
 </html>
